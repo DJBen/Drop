0e2f5a20facee17f58ac167e6f6d1c4b7c95dbcd
DJBen <lsh32768@gmail.com>
2013-09-21 18:30:57 -0400

    Add range indicator for Drop Settings

--- Drop/DRDropSettingsViewController.m
+++ Drop/DRDropSettingsViewController.m
@@ -12,6 +12,8 @@
 
 @interface DRDropSettingsViewController ()
 
+@property (nonatomic, strong) DRDroplet *droplet;
+
 @end
 
 @implementation DRDropSettingsViewController
@@ -30,7 +32,9 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     [self.mapView setRegion:self.originalRegion];
-    [self.mapView addAnnotation:[[DRDroplet alloc] initWithCoordinate:self.originalRegion.center]];
+    self.droplet = [[DRDroplet alloc] initWithCoordinate:self.originalRegion.center];
+    [self.mapView addAnnotation:self.droplet];
+    [self updateRange:1000];
     
     // Delay 1 sec to focus
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1.0 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
@@ -47,16 +51,13 @@
 - (IBAction)cancelButtonTapped:(id)sender {
     [self.delegate dropSettingsCancelled:self];
 }
+
 - (IBAction)dropButtonTapped:(id)sender {
     [self.delegate dropSettingsDone:self droplet:nil];
 }
 
-- (void)focusToCurrentCoordinate {
-    MKCoordinateSpan span = MKCoordinateSpanMake(0.01, 0.01);
-    MKCoordinateRegion region = MKCoordinateRegionMake(self.originalRegion.center, span);
-    [self.mapView setRegion:region animated:YES];
-}
 
+#pragma mark - Map View Delegate
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation {
     static NSString *identifier = @"DRDroplet";
     if ([annotation isKindOfClass:[DRDroplet class]]) {
@@ -74,14 +75,38 @@
     
     return nil;
 }
-- (IBAction)rangeValueChanged:(id)sender {
-    UISlider *slider = (UISlider *)sender;
-    self.rangeLabel.text = [NSString stringWithFormat:@"Range: %.1f km", slider.value / 1000];
-    [self updateRangeVisualRepresentation];
+
+- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
+    if ([overlay isKindOfClass:[MKCircle class]]) {
+        MKCircleRenderer *circle = [[MKCircleRenderer alloc] initWithOverlay:overlay];
+        circle.fillColor = [[UIColor cyanColor] colorWithAlphaComponent:0.15];
+        circle.strokeColor = [[UIColor blueColor] colorWithAlphaComponent:0.5];
+        circle.lineWidth = 1;
+        return circle;
+    }
+    return nil;
 }
 
-- (void)updateRangeVisualRepresentation {
+#pragma mark - View Events
+- (IBAction)rangeValueChanged:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    self.rangeLabel.text = [NSString stringWithFormat:@"Range: %.2f km", slider.value / 1000];
+    [self updateRange:slider.value];
+}
+
+#pragma mark - Helper Methods
+- (void)updateRange:(CGFloat)range {
+    [self.mapView removeOverlays:[self.mapView overlays]];
+    MKCircle *rangeIndicator = [MKCircle circleWithCenterCoordinate:self.droplet.coordinate radius:range];
+    [self.mapView addOverlay:rangeIndicator];
     
+    [self.mapView setRegion:MKCoordinateRegionMakeWithDistance(self.droplet.coordinate, range * 2, range * 2) animated:YES];
+}
+
+- (void)focusToCurrentCoordinate {
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.01, 0.01);
+    MKCoordinateRegion region = MKCoordinateRegionMake(self.originalRegion.center, span);
+    [self.mapView setRegion:region animated:YES];
 }
 
 @end
